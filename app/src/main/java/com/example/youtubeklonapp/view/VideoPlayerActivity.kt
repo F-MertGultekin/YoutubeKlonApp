a0a53c04e87211533c2f1530d3012b73c86e890a
Mert Gultekin <mrtlgltkn.101@gmail.com>
2021-12-09 10:14:36 +0300

    DB FAV

--- app/src/main/java/com/example/youtubeklonapp/view/VideoPlayerActivity.kt
+++ app/src/main/java/com/example/youtubeklonapp/view/VideoPlayerActivity.kt
@@ -4,44 +4,19 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.Toast
-import androidx.lifecycle.ViewModel
 import com.example.youtubeklonapp.BuildConfig
 import com.example.youtubeklonapp.R
-import com.example.youtubeklonapp.viewmodel.VideoPlayerViewModel
-import com.example.youtubeklonapp.viewmodel.ViewModelFactory
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerView
-import androidx.lifecycle.ViewModelProvider
-import com.example.youtubeklonapp.YoutubeKlonApplication
-import com.example.youtubeklonapp.database.VideoDatabase
-import com.example.youtubeklonapp.repository.VideoRepository
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.youtubeklonapp.entitiy.VideoEntitiy
 
-
-class VideoPlayerActivity : AppCompatActivity() {// YouTubeBaseActivity()
+class VideoPlayerActivity : YouTubeBaseActivity() {
 
     lateinit var youTubePlayer: YouTubePlayerView
     lateinit var btnPlayer: Button
     lateinit var youtubePlayerInit: YouTubePlayer.OnInitializedListener
-    lateinit var btnFavorite : ImageButton
-    //lateinit var videoPlayerViewModel : VideoPlayerViewModel
-    //var database: VideoDatabase = VideoDatabase.getDatabase(this)
-    //var repository: VideoRepository = VideoRepository(database.videoDao())
-    //var factory: ViewModelFactory = ViewModelFactory(repository)
-    var uniqueCount : Int = 0
-
-    private val viewModel: VideoPlayerViewModel by viewModels {
-        ViewModelFactory((application as YoutubeKlonApplication).repository)
-    }
-
     val YOUTUBE_API_KEY: String = BuildConfig.API_KEY
     companion object {
         const val VIDEO_ID: String = "VIDEO_ID"
@@ -56,35 +31,18 @@ class VideoPlayerActivity : AppCompatActivity() {// YouTubeBaseActivity()
         }
     }
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_player)
         youTubePlayer= findViewById(R.id.youtubePlayer)
         btnPlayer= findViewById(R.id.btnPlay)
-        btnFavorite = findViewById(R.id.favoriteButton)
 
-        //videoPlayerViewModel = ViewModelProvider(this,factory)[VideoPlayerViewModel::class.java]
+        btnPlayer.setText(intent.getStringExtra(VIDEO_TITLE))
         initUI()
-        btnFavorite.setOnClickListener {
-            val LiveDataSelectedVideo = viewModel.getVideoById(VIDEO_ID)
-            if (LiveDataSelectedVideo != null) {
-                LiveDataSelectedVideo.observe(this, {data ->
-                    data?.let {
-                        viewModel.delete(data)
-                        btnFavorite.setImageDrawable(getDrawable(R.drawable.ic_baseline_favorite_border_24))
-                    }
-                })
-            }
-            else{
-                val videoEntitiy = VideoEntitiy(uniqueCount++, VIDEO_ID,"true")//uniquecount yerine shared peferences kullanılabilir
-                viewModel.insert(videoEntitiy)
-                btnFavorite.setImageDrawable(getDrawable(R.drawable.ic_baseline_favorite_24))
-            }
-
-
-
-        }
     }
+
     private fun initUI() {
         youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
             override fun onInitializationSuccess(
@@ -103,17 +61,5 @@ class VideoPlayerActivity : AppCompatActivity() {// YouTubeBaseActivity()
         btnPlayer.setOnClickListener {
             youTubePlayer.initialize(YOUTUBE_API_KEY,youtubePlayerInit)
         }
-
-    }
-    fun findVideo(videoEntities : List<VideoEntitiy>) : VideoEntitiy?
-    {
-
-        for (video in videoEntities)
-        {
-            if(video.videoId== VIDEO_ID){
-                return video
-            }
-        }
-        return null
     }
 }
